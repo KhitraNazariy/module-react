c3ae189cad57806e11c0b7809c3c808182dfa6bc
KhitraNazariy <khitranazariy19@gmail.com>
2022-01-16 23:45:09 +0200

    bonus-useForm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import Form from "./components/Form/Form";
+import Cars from "./components/Cars/Cars";
+import {useState} from "react";
+
 export default function App() {
+    
+    const [trigger, setTrigger] = useState(null);
+
+    const update = (data) => {
+        setTrigger(data);
+    }
+    
     return (
         <div>
-            App
+            <Form update={update}/>
+            <Cars trigger={trigger}/>
         </div>
     );
 }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,48 @@
+import {useForm} from "react-hook-form";
+import {useState} from "react";
+import {joiResolver} from "@hookform/resolvers/joi";
+
+import {carService} from "../../services/car.service";
+import {CarValidator} from "../../validators/car.validator";
+
+export default function Form({update}) {
+
+    const [formError, setFormError] = useState({});
+
+    const {
+        register, handleSubmit, watch, formState: {errors}
+    } = useForm({resolver: joiResolver(CarValidator)});
+
+    const submit = async (car) => {
+        try {
+            const newCar = await carService.create(car);
+            update(newCar);
+            // console.log(newCar)
+        } catch (errors) {
+            setFormError(errors.response.data)
+        }
+    }
+
+    // const submit = (car) => {
+    //     carService.create(car).then(value => console.log(value)).catch(errors => {
+    //         setFormError(errors.response.data);
+    //     })
+    // }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(submit)}>
+                <div><label>Model:<input type="text" defaultValue={''} {...register('model')}/></label></div>
+                {/*{formError.model && <span>{formError.model[0]}</span>}*/}
+                {errors.model && <span>{errors.model.message}</span>}
+                <div><label>Price:<input type="text" defaultValue={''} {...register('price')}/></label></div>
+                {/*{formError.price && <span>{formError.price[0]}</span>}*/}
+                {errors.price && <span>{errors.price.message}</span>}
+                <div><label>Year:<input type="text" defaultValue={''} {...register('year')}/></label></div>
+                {/*{formError.year && <span>{formError.year[0]}</span>}*/}
+                {errors.year && <span>{errors.year.message}</span>}
+                <button>save</button>
+            </form>
+        </div>
+    );
+}
