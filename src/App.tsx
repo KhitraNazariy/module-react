744980853cbb92a6a1f9c49596cc3716a8e01d9c
KhitraNazariy <khitranazariy19@gmail.com>
2022-02-07 20:31:50 +0200

    redux_toolkit_ts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react';
+
+import Form from "./components/Form/Form";
+
+const App: FC = () => {
+    return (
+        <div>
+            <Form/>
+        </div>
+    );
+};
+
+export default App;
